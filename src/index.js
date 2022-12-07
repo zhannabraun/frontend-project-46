0b853209610905ca0db293c45e9ed42c40a64800
zhannabraun <zhannabraun18@gmail.com>
2022-12-07 22:35:26 +0300

    Fix 'genDiff' function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import path from 'path';
 import compareData from './utilities.js';
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = JSON.parse(fs.readFileSync(path.resolve(filepath1)));
-  const data2 = JSON.parse(fs.readFileSync(path.resolve(filepath2)));
+  const data1 = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf-8'));
   const result = compareData(data1, data2);
 
   return result;
